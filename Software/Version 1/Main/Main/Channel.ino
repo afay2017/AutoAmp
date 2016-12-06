e9755540de350c1b69a928ec1205c49c0ed53b8c
afay2017 <afay2017@francisparker.org>
2016-12-06 13:53:24 -0800

    11/6/16

---
+++ Software/Version 1/Main/Main/Channel.ino
@@ -0,0 +1,11 @@
+class Channel{
+  private int MesaChannel;
+
+  public Channel (){
+    MesaChannel = 1;
+  }
+
+  public int (getMesaChannel){
+    return MesaChannel;
+  }
+}
---
+++ Software/Version 1/Main/Main/Main.ino
@@ -0,0 +1,14 @@
+    #include <Arduino.h>
+    #include "Channel.ino"
+
+Channel channel1;
+
+void setup() {
+  // put your setup code here, to run once:
+  channel1 = new Channel(1);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
