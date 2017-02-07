ccde10db13ca5632733b39d9c5222b6c50969e5f
afay2017 <afay2017@francisparker.org>
2017-02-07 08:46:43 -0800

    2/7/17

--- Software/Version 1/Main/Main/AmpChannel.cpp
+++ Software/Version 1/Main/Main/AmpChannel.cpp
@@ -7,14 +7,12 @@ private:
   bool lead;
   bool mesaEffects;
   bool headEffects;
-  int bypassPin = 1;
-  int headPin = 1;
-  int mesaEffectsPin = 1;
-  int headEffectsPin = 1;
-  int leadPin = 1;
-  int ch2Pin = 1;
-  int ch3Pin = 1;
-  int soloPin = 1;
+
+  double gain;
+  double blend;
+  double tone;
+  double volume;
+
 public:
   AmpChannel(int channelNumber);
   void setAmpChannel(int channel);
@@ -30,6 +28,28 @@ public:
   const static int CRUNCH;
   const static int LEAD;
 
+  void setGain(double value);
+  void setBlend(double value);
+  void setTone(double value);
+  void setVolume(double value);
+
+  double getGain();
+  double getBlend();
+  double getTone();
+  double getVolume();
+
+  void snapToSaved(); //Must be refreshed constnatly
+  void setPointReached();
+
+  int bypassPin = 1;
+  int headPin = 1;
+  int mesaEffectsPin = 1;
+  int headEffectsPin = 1;
+  int leadPin = 1;
+  int ch2Pin = 1;
+  int ch3Pin = 1;
+  int soloPin = 1;
+
 };
 
 const int AmpChannel::ORANGE = 0;
@@ -39,14 +59,14 @@ const int AmpChannel::LEAD   = 2;
 AmpChannel::AmpChannel(int channelNumber)
 {
  this->channel = channelNumber;
- pinMode(bypassPin,OUTPUT);
- pinMode(headPin,OUTPUT);
- pinMode(mesaEffectsPin,OUTPUT);
- pinMode(headEffectsPin,OUTPUT);
- pinMode(leadPin,OUTPUT);
- pinMode(ch2Pin,OUTPUT);
- pinMode(ch3Pin,OUTPUT);
- pinMode(soloPin,OUTPUT);
+ // pinMode(bypassPin,OUTPUT);
+ // pinMode(headPin,OUTPUT);
+ // pinMode(mesaEffectsPin,OUTPUT);
+ // pinMode(headEffectsPin,OUTPUT);
+ // pinMode(leadPin,OUTPUT);
+ // pinMode(ch2Pin,OUTPUT);
+ // pinMode(ch3Pin,OUTPUT);
+ // pinMode(soloPin,OUTPUT);
 
 }
 
@@ -78,25 +98,56 @@ bool AmpChannel::getHeadEffects()
 void AmpChannel::setHead(int head)
 {
  this->head = head;
- switch (head){
-   case ORANGE:
-   digitalWrite(headPin, LOW);
-   break;
-
-   case CRUNCH:
-   digitalWrite(headPin, HIGH);
-   digitalWrite(leadPin, LOW);
-   break;
-
-   case LEAD:
-   digitalWrite(headPin, HIGH);
-   digitalWrite(leadPin, HIGH);
-   break;
-
- }
+ // switch (head){
+ //   case ORANGE:
+ //   digitalWrite(headPin, LOW);
+ //   break;
+ //
+ //   case CRUNCH:
+ //   digitalWrite(headPin, HIGH);
+ //   digitalWrite(leadPin, LOW);
+ //   break;
+ //
+ //   case LEAD:
+ //   digitalWrite(headPin, HIGH);
+ //   digitalWrite(leadPin, HIGH);
+ //   break;
+ // }
 }
 
 int AmpChannel::getHead()
 {
  return (bool)head;
+}
+
+void AmpChannel::setGain(double value){
+  gain = value;
+}
+
+double AmpChannel::getGain(){
+  return gain;
+}
+
+void AmpChannel::setBlend(double value){
+  blend = value;
+}
+
+double AmpChannel::getBlend(){
+  return blend;
+}
+
+void AmpChannel::setTone(double value){
+  tone = value;
+}
+
+double AmpChannel::getTone(){
+  return tone;
+}
+
+void AmpChannel::setVolume(double value){
+  volume = value;
+}
+
+double AmpChannel::getVolume(){
+  return volume;
 }
--- Software/Version 1/Main/Main/Main.ino
+++ Software/Version 1/Main/Main/Main.ino
@@ -1,16 +1,22 @@
     #include <Arduino.h>
     #include <AmpChannel.cpp>
 
-//Channel channel1;
+AmpChannel curChannel;
 
 void setup() {
   // put your setup code here, to run once:
   AmpChannel channel1 = new AmpChannel(1);
+
   pinMode(1,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
 
+void applyPins(){
+  digitalWrite(curChannel.bypassPin,){
 
+  }
+}
 }
