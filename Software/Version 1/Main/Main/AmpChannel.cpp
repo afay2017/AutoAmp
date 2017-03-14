2e21d416d89424b4cca1cdbb27ee106c91f134c8
afay2017 <afay2017@francisparker.org>
2017-03-13 21:06:33 -0700

    3/13/17

--- Software/Version 1/Main/Main/AmpChannel.cpp
+++ Software/Version 1/Main/Main/AmpChannel.cpp
@@ -15,42 +15,139 @@ private:
   double volume;
 
 public:
-  AmpChannel();
-  AmpChannel(int channelNumber);
-  void setAmpChannelNumber(int channel);
-  int getAmpChannelNumber();
-  void setHead(int head);
-  int getHead();
-  void setLead(bool on);
-  bool getLead();
-  void setMesaEffects(bool on);
-  bool getMesaEffects();
-  void setHeadEffects(bool on);
-  bool getHeadEffects();
-  void setBypass(bool on);
-  bool getBypass();
-  int getBypassInt();
+  AmpChannel()
+  {
+   this->channel = 1;
+  }
+  AmpChannel(int channelNumber)
+  {
+   this->channel = channelNumber;
+   // pinMode(bypassPin,OUTPUT);
+   // pinMode(headPin,OUTPUT);
+   // pinMode(mesaEffectsPin,OUTPUT);
+   // pinMode(headEffectsPin,OUTPUT);
+   // pinMode(leadPin,OUTPUT);
+   // pinMode(ch2Pin,OUTPUT);
+   // pinMode(ch3Pin,OUTPUT);
+   // pinMode(soloPin,OUTPUT);
+  }
+  void setAmpChannelNumber(int channelNumber)
+  {
+    if (channelNumber > 3){
+      channelNumber = 3;
+    } else if( channelNumber < 1){
+      channelNumber = 1;
+    }
+   this->channel = channelNumber;
+  }
+
+  int getAmpChannelNumber()
+  {
+   return (int)channel;
+  }
+
+  void setMesaEffects(bool on)
+  {
+   this->mesaEffects = on;
+  }
+
+  bool getMesaEffects()
+  {
+   return (bool)mesaEffects;
+  }
+
+  void setHeadEffects(bool on)
+  {
+   this->headEffects = on;
+  }
+
+  bool getHeadEffects()
+  {
+   return (bool)headEffects;
+  }
+
+  void setBypass(bool on)
+  {
+   this->bypass = on;
+  }
+
+  bool getBypass()
+  {
+   return (bool)bypass;
+  }
+
+  int getBypassInt()
+  {
+   return (int)bypass;
+  }
+
+  void setHead(int head)
+  {
+   this->head = head;
+   // switch (head){
+   //   case ORANGE:
+   //   digitalWrite(headPin, LOW);
+   //   break;
+   //
+   //   case CRUNCH:
+   //   digitalWrite(headPin, HIGH);
+   //   digitalWrite(leadPin, LOW);
+   //   break;
+   //
+   //   case LEAD:
+   //   digitalWrite(headPin, HIGH);
+   //   digitalWrite(leadPin, HIGH);
+   //   break;
+   // }
+  }
+
+  int getHead()
+  {
+   return (int)head;
+  }
+
+  void setGain(double value){
+    gain = value;
+  }
+
+  double getGain(){
+    return gain;
+  }
+
+  void setBlend(double value){
+    blend = value;
+  }
+
+  double getBlend(){
+    return blend;
+  }
+
+  void setTone(double value){
+    tone = value;
+  }
+
+  double getTone(){
+    return tone;
+  }
+
+  void setVolume(double value){
+    volume = value;
+  }
+
+  double getVolume(){
+    return volume;
+  }
   const static int ORANGE = 1;
   const static int CRUNCH = 2;
   const static int LEAD   = 3;
 
-  void setGain(double value);
-  void setBlend(double value);
-  void setTone(double value);
-  void setVolume(double value);
-
-  double getGain();
-  double getBlend();
-  double getTone();
-  double getVolume();
-
   void snapToSaved(); //Must be refreshed constnatly
   void setPointReached();
 
-  int bypassPin = 1;
-  int headPin = 1;
-  int mesaEffectsPin = 1;
-  int headEffectsPin = 1;
+  int bypassPin = 17;
+  int headPin = 16;
+  int mesaEffectsPin = 15;
+  int headEffectsPin = 14;
   int leadPin = 1;
   int ch2Pin = 1;
   int ch3Pin = 1;
@@ -61,128 +158,3 @@ public:
 // const int AmpChannel::ORANGE = 0;
 // const int AmpChannel::CRUNCH = 1;
 // const int AmpChannel::LEAD   = 2;
-
-AmpChannel::AmpChannel(int channelNumber)
-{
- this->channel = channelNumber;
- // pinMode(bypassPin,OUTPUT);
- // pinMode(headPin,OUTPUT);
- // pinMode(mesaEffectsPin,OUTPUT);
- // pinMode(headEffectsPin,OUTPUT);
- // pinMode(leadPin,OUTPUT);
- // pinMode(ch2Pin,OUTPUT);
- // pinMode(ch3Pin,OUTPUT);
- // pinMode(soloPin,OUTPUT);
-
-}
-AmpChannel::AmpChannel()
-{
- this->channel = 1;
-}
-
-void AmpChannel::setAmpChannelNumber(int channelNumber)
-{
-  if (channelNumber > 3){
-    channelNumber = 3;
-  } else if( channelNumber < 1){
-    channelNumber = 1;
-  }
- this->channel = channelNumber;
-}
-
-int AmpChannel::getAmpChannelNumber()
-{
- return (int)channel;
-}
-
-void AmpChannel::setMesaEffects(bool on)
-{
- this->mesaEffects = on;
-}
-
-bool AmpChannel::getMesaEffects()
-{
- return (bool)mesaEffects;
-}
-
-void AmpChannel::setHeadEffects(bool on)
-{
- this->headEffects = on;
-}
-
-bool AmpChannel::getHeadEffects()
-{
- return (bool)headEffects;
-}
-
-void AmpChannel::setBypass(bool on)
-{
- this->bypass = on;
-}
-
-bool AmpChannel::getBypass()
-{
- return (bool)bypass;
-}
-
-int AmpChannel::getBypassInt()
-{
- return (int)bypass;
-}
-
-void AmpChannel::setHead(int head)
-{
- this->head = head;
- // switch (head){
- //   case ORANGE:
- //   digitalWrite(headPin, LOW);
- //   break;
- //
- //   case CRUNCH:
- //   digitalWrite(headPin, HIGH);
- //   digitalWrite(leadPin, LOW);
- //   break;
- //
- //   case LEAD:
- //   digitalWrite(headPin, HIGH);
- //   digitalWrite(leadPin, HIGH);
- //   break;
- // }
-}
-
-int AmpChannel::getHead()
-{
- return (bool)head;
-}
-
-void AmpChannel::setGain(double value){
-  gain = value;
-}
-
-double AmpChannel::getGain(){
-  return gain;
-}
-
-void AmpChannel::setBlend(double value){
-  blend = value;
-}
-
-double AmpChannel::getBlend(){
-  return blend;
-}
-
-void AmpChannel::setTone(double value){
-  tone = value;
-}
-
-double AmpChannel::getTone(){
-  return tone;
-}
-
-void AmpChannel::setVolume(double value){
-  volume = value;
-}
-
-double AmpChannel::getVolume(){
-  return volume;
-}
--- Software/Version 1/Main/Main/Main.ino
+++ Software/Version 1/Main/Main/Main.ino
@@ -1,29 +1,68 @@
     #include <Arduino.h>
     #include <AmpChannel.cpp>
 
-AmpChannel curChannel;
+ AmpChannel curChannel;
+ AmpChannel channel1;
+ AmpChannel channel2;
 
 void setup() {
   // put your setup code here, to run once:
   curChannel = new AmpChannel((int)1);
+  channel1 = new AmpChannel(1);
+  channel1 = new AmpChannel(2);
 
-  pinMode(1,OUTPUT);
+  channel1.setHead(curChannel.ORANGE);
+  channel2.setHead(curChannel.ORANGE);
+
+  channel1.setMesaEffects(false);
+  channel2.setMesaEffects(false);
+
+  channel1.setHeadEffects(true);
+  channel2.setHeadEffects(true);
+
+
+
+  Serial.begin(9600);
+
+  pinMode(8,INPUT_PULLUP);
+  pinMode(9,INPUT_PULLUP);
+
+  pinMode(curChannel.headPin,OUTPUT);
+  pinMode(curChannel.bypassPin,OUTPUT);
+  pinMode(curChannel.headEffectsPin,OUTPUT);
+  pinMode(curChannel.mesaEffectsPin,OUTPUT);
+
+
+  //Serial.println("OFF");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (digitalRead(8)==HIGH){
+    //digitalWrite(curChannel.bypassPin, LOW);
+    curChannel = channel1;
+    applyPins();
+    //Serial.println("OFF");
+  } else {
+    curChannel = channel2;
+    applyPins();
+    //Serial.println("ON");
+    //digitalWrite(curChannel.bypassPin, HIGH);
+  }
 }
 
 void applyPins(){
-  digitalWrite(curChannel.bypassPin,curChannel.getBypassInt());
+  //digitalWrite(curChannel.bypassPin,curChannel.getBypassInt());
+  Serial.println(curChannel.getHead());
   switch (curChannel.getHead()){
     case curChannel.ORANGE:
     digitalWrite(curChannel.headPin, LOW);
+    Serial.println("ORANGE");
     break;
 
     case curChannel.CRUNCH:
     digitalWrite(curChannel.headPin, HIGH);
     digitalWrite(curChannel.leadPin, LOW);
+    Serial.println("CRUNCH");
     break;
 
     case curChannel.LEAD:
@@ -35,7 +74,7 @@ void applyPins(){
   digitalWrite(curChannel.headEffectsPin,curChannel.getHeadEffects());
   digitalWrite(curChannel.mesaEffectsPin,curChannel.getMesaEffects());
 
-  applyMesaChannelPins(curChannel.getAmpChannelNumber());
+//  applyMesaChannelPins(curChannel.getAmpChannelNumber());
 
 }
 
